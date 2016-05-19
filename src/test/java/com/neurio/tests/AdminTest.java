54001713a71eabf8045dd8c293b10b58264c1d99
Robert T <robert@neur.io>
2016-05-18 17:35:30 -0700

    - Completed the Admin Test

--- src/test/java/com/neurio/tests/AdminTest.java
+++ src/test/java/com/neurio/tests/AdminTest.java
@@ -1,10 +1,27 @@
 package com.neurio.tests;
 
-import static org.testng.Assert.*;
+import com.neurio.tests.shared.BasicTest;
+import com.neurio.tests.shared.UserBar;
+import com.neurio.tests.shared.LoginPage;
+import org.testng.annotations.Test;
 
 /**
- * Created by robert on 2016-05-17.
+ * Created by Robert on 2016-05-17.
+ * Admin Test
  */
-public class AdminTest {
+public class AdminTest extends BasicTest{
 
+    @Test
+    public void AdminTest() {
+        String ADMIN_LOGIN = "admin@energy-aware.com";
+        String ADMIN_PASSWORD = "bonny5_worktable";
+        String WINDSOR = "windsor";
+        String WINDSOR_ID = "yVqQsLETRk2C1D1EjlgEjA";
+
+        LoginPage.signIn(ADMIN_LOGIN, ADMIN_PASSWORD);
+
+        UserBar.toggleUserMenu();
+
+        UserBar.adminSelectUserByNameID(WINDSOR, WINDSOR_ID);
+    }
 }
